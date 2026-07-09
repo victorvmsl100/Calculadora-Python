406ab2b650f0f074b018f52f7b3ccdf63a0de9b3
victorvmsl100 <victorvmsl100@gmail.com>
2026-07-09 16:29:37 -0300

    Calculadora Python

---
+++ calculadora.py
@@ -0,0 +1,31 @@
+# criar uma calculadora simples que pergunte meu nome 
+# Passo 1: Saber o nome do usuário
+# Passo 2: Perguntar os números para calcular
+# Passo 3: Exibir o resultado 
+print("===================================")
+print("Projeto 01 - Calculadora Simples em Python")
+print("Calculadora do Victor ")
+print("Julho de 2026")
+print("====================================")
+
+nome = input("Digite seu nome: ")
+print("Olá, " + nome + "! Bem-vindo à calculadora.")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+resultado = num1 + num2
+print("Soma:", resultado) 
+
+resultado = num1 - num2
+print("Subtração:", resultado)
+
+resultado = num1 * num2
+print("Multiplicação:", resultado)
+
+resultado = num1 / num2
+print("Divisão:", resultado)
+
+
+
+
+
